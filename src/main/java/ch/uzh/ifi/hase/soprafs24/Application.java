ce928c7b3de2f0e0021c85e06ba61f8be21d23d7
LaughingF0x <115981865+LaughingF0x@users.noreply.github.com>
2024-03-08 17:23:11 +0100

    fixed server display

--- src/main/java/ch/uzh/ifi/hase/soprafs24/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/Application.java
@@ -3,6 +3,7 @@ package ch.uzh.ifi.hase.soprafs24;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@RestController
+
 @SpringBootApplication
 public class Application {
 
