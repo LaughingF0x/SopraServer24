5c59473bd8048ba830cee8ec91e1f199fba7906e
LaughingF0x <115981865+LaughingF0x@users.noreply.github.com>
2024-03-08 17:17:28 +0100

    fixed server display

--- src/main/java/ch/uzh/ifi/hase/soprafs24/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/Application.java
@@ -1,6 +1,5 @@
 package ch.uzh.ifi.hase.soprafs24;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @RestController
 @SpringBootApplication
